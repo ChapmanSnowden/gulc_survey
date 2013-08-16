ee741f15b372707c911476116421e42f30110f19
DBC-Apprentice <dbc@dbc.com>
2013-08-16 16:33:14 -0700

    dynamic forms using ajax w/o end button

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-//create an event handler on the create user submit
-//find email and validate
-  // produce erros messages
-//find password and validate
-  // produce erros messages
-  // function checkOrRemoveErrorForEmail(){
 
   $('#create_form').on('submit', function(event) {
     
@@ -41,4 +35,35 @@ $(document).ready(function() {
     };
 
   });
+
+  $('#create_survey').on('submit', function(evt){
+    evt.preventDefault();
+    var survey_name = $(this).serialize();
+    var survey_creation = $.ajax({
+      type: this.method,
+      url: this.action,
+      data: survey_name
+    });
+
+    survey_creation.done(function(response) {
+      $('.container').append(response);
+    });
+
+  });
+
+  $('#add_question').on('submit', function(evt){
+    evt.preventDefault();
+    var question_data = $(this).serialize();
+    var next_question = $.ajax({
+      type: this.method,
+      url: this.action,
+      data: question_data
+    });
+
+    next_question.done(function(response){
+      $('.container').append(response);
+    });
+
+  });
+
 });
