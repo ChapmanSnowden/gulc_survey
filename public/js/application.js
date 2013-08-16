bd33f4dcfbc0a807fb3c995f4e5b306d6506c102
Apprentice <apprentice@dbc15.local>
2013-08-16 16:39:31 -0700

    implement javascript client-side sign-up validation

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,44 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+//create an event handler on the create user submit
+//find email and validate
+  // produce erros messages
+//find password and validate
+  // produce erros messages
+  // function checkOrRemoveErrorForEmail(){
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#create_form').on('submit', function(event) {
+    
+    $('#errors li').remove();
+
+     var errors = []
+
+     var email = $('#create_form input[name="email"]').val();
+     var password = $('#create_form input[name="password"]').val();
+     
+    
+    if ((/(.+)@(.+){2,}\.(.+){2,}/).test(email) === false) {
+      $('#errors').append("<li> Must be a valid email </li>");
+      errors.push("Bad email");
+    };
+ 
+    if ( (/[\d]/).test(password) === false) {
+      $('#errors').append("<li> Password must have at least 1 numeric character </li>");
+      errors.push("No numbers");
+    };
+
+    if ( (/[A-Z]/).test(password) === false) {
+      $('#errors').append("<li> Password must have at least 1 uppercase letter </li>");
+      errors.push("No uppercase");
+    };
+
+    if ( (/.{8,}/).test(password) === false) {
+      $('#errors').append("<li> Password must have at least 8 characters </li>");
+      errors.push("Too short");
+    };
+    
+    if (errors.length > 0){
+      event.preventDefault();
+    };
+
+  });
 });
