ed682160bdf2f58e504b661d7c7923e39df09940
Apprentice <apprentice@dbc15.local>
2013-08-16 15:38:54 -0700

    complete user authentication process, setting session

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,4 @@
 get '/' do
-  @grandma = params[:grandma]
-  # Look in app/views/index.erb
   erb :index
 end
 
-post '/grandma' do
-  "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
 class User < ActiveRecord::Base
   has_many :surveys
 
+  include BCrypt
   
+  def password
+    @password ||= Password.new(password_hash)
+  end
+  
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)
+    return nil if user.nil?
+    
+    user.password == password ? user : nil
+  end
+
 end
