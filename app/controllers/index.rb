31c644b37fb441196173b288afff6caf2405fb9f
DBC-Apprentice <dbc@dbc.com>
2013-08-16 12:31:24 -0700

    create survey form and route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,13 @@
 get '/' do
-  @grandma = params[:grandma]
-  # Look in app/views/index.erb
+  
   erb :index
 end
 
-post '/grandma' do
-  "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+get '/dashboard' do
+  erb :dashboard
+end
+
+post '/surveys' do
+  p params[:name]
+  p params[:prompt]
 end
