2f6fe71d6b88d3eb549b70482603fde8afcaf1df
DBC-Apprentice <dbc@dbc.com>
2013-08-17 22:17:57 -0700

    slight refactoring

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,18 +2,19 @@ get '/' do
   @errors = session.delete(:error_message) || []
   @user = User.find(session[:user_id]) if session[:user_id]
   @all_surveys = Survey.all || []
+
   if @user
     @unanswered_surveys = (@all_surveys - Survey.where(:id => @user.completed_surveys.map(&:survey_id)))
     erb :dashboard
   else
     erb :index
   end
+
 end
 
 post '/create' do
   if taken?(params[:email])
     session[:error_message] = ["That email address has already been registered."]
-    redirect '/'
   else
     @user = User.new(name: params[:name], email: params[:email])
     @user.password = params[:password]
@@ -21,25 +22,20 @@ post '/create' do
 
     session[:user_id] = @user.id
 
-    redirect '/'
   end
+
+  redirect '/'
 end
 
 post '/login' do
   user = User.authenticate(params[:email], params[:password])
-  puts params[:email]
-  puts params[:password]
-  puts User.authenticate(params[:email], params[:password])
-  if user   
-    session[:user_id] = user.id
-    redirect '/'
-  else
-    redirect '/'
-  end
+  session[:user_id] = user.id if user   
 
+  redirect '/'
 end
 
 get '/logout' do 
   session.clear
+
   redirect '/'
 end
