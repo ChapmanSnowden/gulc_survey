2703938be475d6d4773b46988bb48dcd127f9d7c
George Pradhan <kgeorgepradhan@gmail.com>
2013-08-17 17:36:23 -0700

    fix index page validations

--- public/js/user_account.js
+++ public/js/user_account.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
 
-$('#create_form').on('submit', function(event) {
-    
+$('.jumbotron form').on('submit', function(event) {
+    console.log('hi');
     $('#errors li').remove();
 
      var errors = []
 
      var password = $('#exampleInputPassword1').val();
-     
-    
+         
     if ( (/[\d]/).test(password) === false) {
       $('#errors').append("<li> Password must have at least 1 numeric character </li>");
       errors.push("No numbers");
